c18def74e03af88bad1391bc6816661a926e5819
lennertsteyaert <lennert_steyaert@hotmail.com>
2020-03-16 11:28:26 +0100

    Add loginview

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,7 @@
+import uuid
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 #################################################################
 #///////////////////////////////////////////////////////////////#
@@ -41,3 +43,87 @@ class IO(models.Model):
     stateText = models.CharField(max_length=32)                         # If the type had a string value this field represent that value
     stateDecimal = models.DecimalField(max_digits=20, decimal_places=2) # If the type had an decimal value this field represent that value
     device = models.ForeignKey(Device, on_delete=models.CASCADE)        # I am a IO and I belong to a device...
+
+############################################################################################################################
+# User stuff below (https://medium.com/analytics-vidhya/django-rest-api-with-json-web-token-jwt-authentication-69536c01ee18)
+############################################################################################################################
+
+class UserManager(BaseUserManager):
+    '''
+    creating a manager for a custom user model
+    https://docs.djangoproject.com/en/3.0/topics/auth/customizing/#writing-a-manager-for-a-custom-user-model
+    https://docs.djangoproject.com/en/3.0/topics/auth/customizing/#a-full-example
+    '''
+    def create_user(self, email, password=None):
+        """
+        Create and return a `User` with an email, username and password.
+        """
+        if not email:
+            raise ValueError('Users Must Have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Create and return a `User` with superuser (admin) permissions.
+        """
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+
+class User(AbstractBaseUser):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True
+        )
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    # Tells Django that the UserManager class defined above should manage
+    # objects of this type.
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        '''
+        to set table name in database
+        '''
+        db_table = "login"
+
+class UserProfile(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    first_name = models.CharField(max_length=50, unique=False)
+    last_name = models.CharField(max_length=50, unique=False)
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
+    class Meta:
+        '''
+        to set table name in database
+        '''
+        db_table = "profile"
--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import UserRegistrationView
 
 urlpatterns = [
     path('',views.index,name='index'),
@@ -9,6 +11,7 @@ urlpatterns = [
     path('device',views.device,name='device'),
     path('io',views.io,name='io'),
     path('deviceIO',views.deviceIO,name='deviceIO'),
+    url(r'^signup', UserRegistrationView.as_view()),
     
  #   path('device/<int:id>',views.device,name='device'),
 ]
